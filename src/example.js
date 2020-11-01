440fb0ebc5ffd1369bea4bb9b79dcfd8985089f5
fedodd <fedodd.s@gmail.com>
2020-11-01 10:40:31 +0300

    concat function

---
+++ src/example.js
@@ -0,0 +1,6 @@
+//concat function
+function concat(firstStr, secondStr) {
+    return firstStr + secondStr;
+}
+var result = concat('Hello ', 'World');
+console.log(result);
---
+++ src/example.ts
@@ -0,0 +1,7 @@
+//concat function
+
+function concat(firstStr: string, secondStr: string): string {
+    return firstStr + secondStr
+}
+
+let result = concat('Hello ', 'World');
