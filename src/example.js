ab3c3df29e0ede289718f5d7cd9a9ac5b8a56a9c
fedodd <fedodd.s@gmail.com>
2020-11-01 10:53:14 +0300

    add interface and fix function type

--- src/example.js
+++
@@ -1,6 +0,0 @@
-//concat function
-function concat(firstStr, secondStr) {
-    return firstStr + secondStr;
-}
-var result = concat('Hello ', 'World');
-console.log(result);
--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,23 @@
 //concat function
 
-function concat(firstStr: string, secondStr: string): string {
+type typeConcatFn = (firstStr: string, secondStr: string) => string ;
+
+const concat: typeConcatFn = (firstStr: string, secondStr: string) => {
     return firstStr + secondStr
 }
 
 let result = concat('Hello ', 'World');
+
+//interface
+
+interface homeTask {
+    howIDoIt: string,
+	simeArray: (number | string)[]
+	withData: object[],
+}
+
+const MyHometask: homeTask  = {
+	howIDoIt: "I Do It Wel",
+	simeArray: ["string one", "string two", 42],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
