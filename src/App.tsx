fd5c79361cd0e4669d6e77fe3d98375541281742
fedodd <fedodd.s@gmail.com>
2020-11-02 21:08:42 +0300

    Add Eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import cn from "classnames";
-import style from "./App.modules.scss";
-import "./custom.css";
-
+import cn from 'classnames';
+import style from './App.module.scss';
 
 const App = () => {
+  console.log('###: Some log');
+
   return (
     <div>
-      <h1 className={cn(style.header, "color")}>This is App Component!</h1>
+      <h1 className={cn(style.header, 'color')}>This is App Component!</h1>
     </div>
   );
 };
