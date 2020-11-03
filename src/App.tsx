f06123237d30fb4cf7f9eb4e26d0e82af943b810
fedodd <fedodd.s@gmail.com>
2020-11-03 20:38:52 +0300

    add sxtensions prettier and eslint and format on save

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,11 @@ import cn from 'classnames';
 import style from './App.module.scss';
 
 const App = () => {
-  console.log('###: Some log');
-  console.log('###: Some log 2');
-
   return (
     <div>
-
-
-
       <h1 className={cn(style.header, 'color')}>This is App Component!</h1>
     </div>
   );
-
-
-
-
-
 };
 
 export default App;
