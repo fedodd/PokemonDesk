d4e5555a67a9edef8fc9d602d8a13bf388856a2b
fedodd <fedodd.s@gmail.com>
2020-11-08 17:06:14 +0300

    add parallax

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Button from '../../components/Button/Button';
+import Parallax from '../../components/Parallax/Parallax';
 
 import style from './Home.module.scss';
 
@@ -11,8 +12,16 @@ const Home = () => {
     <div className={style.root}>
       <Header />
       <div className={style.main}>
-        <h1 className={style.title}>This is Home Component!</h1>
-        <Button onClick={(event) => console.log('clicked!')}>See pokemons</Button>
+        <div>
+          <h1 className={style.title}>
+            <strong>Find</strong> all your favorite <strong>Pokemon</strong>
+          </h1>
+          <p className={style.description}>
+            You can know the type of Pokemon, its strengths, disadvantages and abilities
+          </p>
+          <Button onClick={(event) => console.log('clicked!')}>See pokemons</Button>
+        </div>
+        <Parallax />
       </div>
       <Footer />
     </div>
